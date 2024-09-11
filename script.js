2f263bffd2bb2e8b66a35c5e8511c570f4949f74
grupo006efg <164170558+grupo006efg@users.noreply.github.com>
2024-09-11 11:13:02 -0300

    versao 0.0.1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function validadefotm() {
+    var email=document.getElementById(email).ariaValueMax;
+    varpassword=document.getElementById(password).ariaValueMax
+if(email===""||password===) {
+alert("por favor,prencha todos os campos.");
+return false;
 
+}
+//Adicione validações adicionais aqui;se necessário
+return true
+    
+}
